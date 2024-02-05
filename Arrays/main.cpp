692f085bdad4545c829c6fdcb21a4c76d1fe1586
Lapin_qh81@top-academy.ru <Lapin_qh81@top-academy.ru>
2024-02-05 19:45:00 +0900

    Shift and arrays tasks are done.

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,18 +1,106 @@
 ﻿//Arrays
 #include <iostream>
+#include "windows.h"
+
 using namespace std;
 
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int n = 5;
-	int arr[n] = {3, 5, 8};
-	//arr[1] = 1024;  //Обращение к элементу массива на запись.
-	//arr[3] = 3072;
-	//cout << arr[1] << endl; //Обращение к элементу массива на чтение.
-	for (int i = 0; i < n; i++)
+	//const int n = 5;
+	//int arr[n] = {3, 5, 8};
+	////arr[1] = 1024;  //Обращение к элементу массива на запись.
+	////arr[3] = 3072;
+	////cout << arr[1] << endl; //Обращение к элементу массива на чтение.
+	//for (int i = 0; i < n; i++)
+	//{
+	//	cout << arr[i] << "\t";
+	//}
+	//cout << endl;
+
+
+	int choose = 0;
+	//Создаем массив
+	const int size = 5;
+	int array[size]{ 0,0,0,0,0 };
+
+	//Делаем цикл для записи значений массива
+	for (int i = 0; i < size; i++)
 	{
-		cout << arr[i] << "\t";
+		cout << "Введите значения для эллемента массива "<< i <<": ";
+		cin >> array[i];
 	}
 	cout << endl;
+	cout << "Выбирете действие:" << endl;
+	cout << "1. Вывести эллемента массива в прямом порядке." << endl;
+	cout << "2. Вывести эллементы массива в обратном порядке." << endl;
+	cout << "3. Посчитать сумму всех эллементов массива." << endl;
+	cout << "4. Посчитать средне-арифметическое всех эллементов массива." << endl;
+	cout << "5. Найти минимальный и максимальный эллемент массива." << endl;
+	cin >> choose;
+
+		//Цикл для вывода значений в прямом порядке
+	if (choose == 1)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			cout << array[i] << "\t";
+		}			
+	}
+
+		//Цикл для вывода массива в обратном порядке
+	else if (choose == 2)
+	{
+		for (int i = size-1; i >= 0; i--)
+		{
+			cout << array[i] << "\t";
+		}
+	}
+
+		//Блок для суммирования эллементов массива
+	else if (choose == 3)
+	{
+		cout << array[0] + array[1] + array[2] + array[3] + array[4] << endl;
+	}
+
+		//Блок для нахождения средне-арифметического эллементов массива
+	else if (choose == 4)
+	{
+		cout << (array[0] + array[1] + array[2] + array[3] + array[4]) / size << endl;
+	}
+
+		//Блок определения минимального и максимального значения эллементов.
+	else if (choose == 5)
+	{
+		int min = array[0];
+		int max = array[0];
+
+		for (int i = 0; i < size; i++)
+		{
+			if (min > array[i])
+			{
+				min = array[i];
+			}
+			if (max < array[i])
+			{
+				max = array[i];
+			}
+		}
+		cout << "Минимальное значение равно " << min << endl;
+		cout << "Максимально значние равно " << max << endl;
+	}
+
+	else
+	{
+		cout << "Неправильно задан параметр.";
+		cout << endl;
+		for (int i = 0; i < 25; i++)
+		{
+			Sleep(250);
+			cout << ".";
+		}
+		cout << endl;
+		cout << "Прекращение работы программы.";
+	}
+	
 }
---
+++ Shift/main.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "ru");
+
+	//Создаем массив.
+	const int size = 10;
+	int array[size] = { 0,1,1,2,3,5,8,13,21,34 };
+
+	//Запрашиваем операцию.
+	int choose;
+	cout << "Выберете сторону сдвига" << endl;
+	cout << "1.Влево" << endl;
+	cout << "2.Вправо" << endl;
+	cin >> choose;
+
+	//Запрашиваем число сдвига.
+	int move_number = 0;
+	cout << "Введите число сдвига: ";
+	cin >> move_number;
+	cout << endl;
+
+
+	//Выводим оригинльный массив для сравнения
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i] << "\t";
+	}
+	cout << endl;
+
+
+	//Делаем сдвиг влево.
+	if (choose == 1)
+	{
+		int first_array_value;
+		for (int i = 0; i < move_number; i++)
+		{
+			first_array_value = array[0];
+			for (int j = 0; j < size - 1; j++)
+			{
+				array[j] = array[j + 1];
+			}
+			array[size - 1] = first_array_value;
+
+			for (int i = 0; i < size; i++)
+			{
+				cout << array[i] << "\t";
+			}
+			cout << endl;
+		}
+	}
+
+	//Делаем сдвиг вправо.
+	if (choose == 2)
+	{
+		int last_array_value;
+		for (int i = 0; i < move_number; i++)
+		{
+			last_array_value = array[size - 1];
+			for (int j = size - 2; j >= 0; j--)
+			{
+				array[j + 1] = array[j];
+			}
+			array[0] = last_array_value;
+
+			for (int i = 0; i < size; i++)
+			{
+				cout << array[i] << "\t";
+			}
+			cout << endl;
+		}
+	}
+}
